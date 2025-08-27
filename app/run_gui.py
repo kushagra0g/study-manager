47bf2e95b521137cb0a3466e86c75c66aa06b49b
creeper <kushgupta491@gmail.com>
2025-08-27 23:56:26 +0530

    improved UI

--- app/run_gui.py
+++ app/run_gui.py
@@ -9,12 +9,17 @@ def main():
         for subject in subjects:
             b = CTkButton(master=subject_frame,
                           text=subject[0],
+                          text_color="#000814",
+                          fg_color="#FFC300",
                           font=("Bahnschrift", 17),
                           command=lambda name=subject: show_tasks_for_subject(name))
             b.pack(padx=30, pady=10)
 
-        CTkButton(master=subject_frame, text="New Subject", font=("Bahnschrift", 17), fg_color="black",
-                  command=add_subject_button_handler).pack(padx=30, pady=40,
+        CTkButton(master=subject_frame,
+                  text="New Subject",
+                  font=("Bahnschrift", 17),
+                  fg_color="#000814",
+                  command=add_subject_button_handler).pack(padx=10, pady=40,
                                                            side="bottom")
 
     def show_tasks_for_subject(subject_name: tuple):
@@ -40,23 +45,34 @@ def main():
             if subject_name:
                 add_subject(subject_name)
                 entry.delete(0, "end")
-                CTkLabel(master=task_frame, text=f"New subject added successfully ", text_color="green",
+                CTkLabel(master=task_frame,
+                         text=f"New subject added successfully ",
+                         text_color="green",
                          font=("Arial", 20)).pack(padx=20, pady=20)
                 render_subject_frame()
                 show_tasks_for_subject(subject_name)
 
             else:
-                CTkLabel(master=task_frame, text="Subject name cannot be empty! ", text_color="red",
+                CTkLabel(master=task_frame,
+                         text="Subject name cannot be empty! ",
+                         text_color="red",
                          font=("Arial", 20)).pack(padx=20, pady=20)
 
-        entry = CTkEntry(master=task_frame, placeholder_text="Enter New subject name")
+        entry = CTkEntry(master=task_frame,
+                         placeholder_text="Enter New subject name",
+                         border_color="#FFC300",
+                         font=("Bahnschrift", 11))
         entry.pack(padx=20, pady=20)
-        CTkButton(master=task_frame, text="Add Subject", command=handle_add_subject).pack(padx=20, pady=20)
+        CTkButton(master=task_frame,
+                  text="Add Subject",
+                  font=("Bahnschrift", 17),
+                  fg_color="#000814",
+                  command=handle_add_subject).pack(padx=20, pady=20)
 
     def add_task_button_handler():
-        # to make
+        # to develop
         ...
-    app = CTk()
+    app = CTk(fg_color="#000814")
     app.title("Study Manager")
     set_appearance_mode("dark")
     app.geometry("700x500")
@@ -67,13 +83,21 @@ def main():
     app.grid_columnconfigure(2, weight=1)
 
     # Setting up Subjects frame
-    subject_frame = CTkScrollableFrame(master=app)
+    subject_frame = CTkScrollableFrame(master=app,
+                                       fg_color="#001D3D",
+                                       scrollbar_button_color="#000814",
+                                       scrollbar_button_hover_color="#003566"
+                                       )
     subject_frame.grid(row=0, column=0, padx=20, pady=20, sticky="nsw")
 
     # Setup of tasks frame
-    task_frame = CTkFrame(master=app)
+    task_frame = CTkFrame(master=app,
+                          fg_color="#001D3D")
     task_frame.grid(row=0, column=2, padx=20, pady=20, sticky="nsew")
 
     render_subject_frame()
 
     app.mainloop()
+
+if __name__ == "__main__":
+    main()
